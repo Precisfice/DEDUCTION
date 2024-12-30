d5e3826aa810ce721fee9f24cdcdd7f60f2da32b
David C. Norris <david@precisionmethods.guru>
2024-12-29 19:47:37 -0500

    qs_d_nmax/3 solves MGQ, simplifying meet_invalid/3

--- tally.pl
+++ tally.pl
@@ -1,10 +1,12 @@
 % tally.pl
 % Basic utilities for tally generation, esp. fair enumeration
-/*
+
 :- module(tally, [
               qs_d_nmax/3
+              ,n_d_max/3
+              ,n_d_sum/3
 	  ]).
-*/
+
 :- use_module(library(lists)).
 :- use_module(library(clpz)).
 :- use_module(library(lambda)).
@@ -12,21 +14,54 @@
 
 clpz:monotonic.
 
-% Regions near the origin
-% TODO: Find a name conveying geometrical intutition ('sphere'? 'hypercube'?),
-%       or perhaps link this to *accessible* tallies as discussed in Fact 1.27.
-% TODO: Also deal with the 'qs' plurality; perhaps in this module, a given tally
-%       counts as 'q', and only _lists_ of tallies should be regarded a plural?
-%       Alternatively, perhaps there will be 'low-level' predicates that regard
-%       a tally as a list, properly called 'Qs', but 'higher-level' predicates
-%       that regard a 'Tally' or 'Tallies' at higher levels of abstraction.
-%       (Of course, in general these distinct manners of regarding tallies may
-%       well argue for distinct modules!)
+% Having achieved fair enumeration for ℕ¹ ⊔ ℕ² ⊔ ... below,
+% we now generalize qs_d_nmax/3 to support the MGQ.
+
 qs_d_nmax(Qs, D, Nmax) :-
-    length(Qs, D),
-    maplist(\Q^T^N^(Q = T/N), Qs, Ts, Ns),
-    Ns ins 0..Nmax, label(Ns),
-    maplist(\T^N^(T in 0..N), Ts, Ns), label(Ts).
+    n_d_max(Ns, D, Nmax),
+    length(Ts, D),
+    maplist(\T^N^(T in 0..N), Ts, Ns),
+    label(Ts),
+    maplist(\Q^T^N^(Q = T/N), Qs, Ts, Ns).
+
+%?- L+\(findall(Qs, qs_d_nmax(Qs, 2, 3), Qss), length(Qss, L)).
+%@    L = 100. % Correct: (4+3+2+1)^2
+
+%?- qs_d_nmax(Qs, D, Nmax). % MGQ
+%@    Qs = [0/0], D = 1, Nmax = 0
+%@ ;  Qs = [0/0], D = 1, Nmax = 1
+%@ ;  Qs = [0/1], D = 1, Nmax = 1
+%@ ;  Qs = [1/1], D = 1, Nmax = 1
+%@ ;  Qs = [0/0,0/0], D = 2, Nmax = 0
+%@ ;  Qs = [0/0], D = 1, Nmax = 2
+%@ ;  Qs = [0/1], D = 1, Nmax = 2
+%@ ;  Qs = [1/1], D = 1, Nmax = 2
+%@ ;  Qs = [0/2], D = 1, Nmax = 2
+%@ ;  Qs = [1/2], D = 1, Nmax = 2
+%@ ;  Qs = [2/2], D = 1, Nmax = 2
+%@ ;  Qs = [0/0,0/0], D = 2, Nmax = 1
+%@ ;  Qs = [0/0,0/1], D = 2, Nmax = 1
+%@ ;  Qs = [0/0,1/1], D = 2, Nmax = 1
+%@ ;  Qs = [0/1,0/0], D = 2, Nmax = 1
+%@ ;  Qs = [1/1,0/0], D = 2, Nmax = 1
+%@ ;  Qs = [0/1,0/1], D = 2, Nmax = 1
+%@ ;  Qs = [0/1,1/1], D = 2, Nmax = 1
+%@ ;  Qs = [1/1,0/1], D = 2, Nmax = 1
+%@ ;  Qs = [1/1,1/1], D = 2, Nmax = 1
+%@ ;  Qs = [0/0,0/0,0/0], D = 3, Nmax = 0
+%@ ;  Qs = [0/0], D = 1, Nmax = 3
+%@ ;  Qs = [0/1], D = 1, Nmax = 3
+%@ ;  Qs = [1/1], D = 1, Nmax = 3
+%@ ;  Qs = [0/2], D = 1, Nmax = 3
+%@ ;  Qs = [1/2], D = 1, Nmax = 3
+%@ ;  Qs = [2/2], D = 1, Nmax = 3
+%@ ;  Qs = [0/3], D = 1, Nmax = 3
+%@ ;  Qs = [1/3], D = 1, Nmax = 3
+%@ ;  Qs = [2/3], D = 1, Nmax = 3
+%@ ;  Qs = [3/3], D = 1, Nmax = 3
+%@ ;  Qs = [0/0,0/0], D = 2, Nmax = 2
+%@ ;  Qs = [0/0,0/1], D = 2, Nmax = 2
+%@ ;  ... .
 
 % TODO:
 % I would really like to dispense with explicit enumeration,
