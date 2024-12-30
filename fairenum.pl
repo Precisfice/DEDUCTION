6b102ef66ccef0bf658beb32b77511addc92fe26
David C. Norris <david@precisionmethods.guru>
2024-12-30 17:43:43 -0500

    Improved fair enumeration

--- fairenum.pl
+++ fairenum.pl
@@ -10,60 +10,223 @@
 :- module(fairenum, [
               n_d_max/3
               ,n_d_sum/3
+              % Pending https://github.com/mthom/scryer-prolog/issues/2547,
+              % we cannot export some pretty operators.  Client modules may
+              % however access these by explicit qualification, and assign
+              % these operators locally.
+              ,op(900, xfx, '≤') % Mutually exclusive infix
+              ,op(900, xfx, '≰') % relations defined on ℕᴰ.
+              ,op(900, xfx, '≠') % Generate distinct pairs.
 	  ]).
 
 :- use_module(library(lists)).
 :- use_module(library(clpz)).
 :- use_module(library(lambda)).
-:- use_module(library(time)).
+:- use_module(library(reif)).
+:- use_module(library(debug)).
+:- use_module(library(format)).
+:- use_module(comprehension).
 
 clpz:monotonic.
 
+:- op(900, xfx, '≤'). % Mutually exclusive infix
+:- op(900, xfx, '≰'). % relations defined on ℕᴰ.
+:- op(900, xfx, '≠'). % Generate distinct pairs from ℕᴰ.
+
+% ('≤')/3 does NOT attempt fair enumeration
+'≤'([], [], true). % trivial case makes general clause easier to implement
+'≤'([X|Xs], [Y|Ys], Truth) :- % ≤ extended to ℕᴰ, D≥1
+    if_(clpz_t(#X #=< #Y),
+        '≤'(Xs,Ys,Truth),
+        Truth = false
+       ).
+
+%?- '≤'([], [], Truth).
+%@    Truth = true. % A quirk easily excluded from ('≤')/2
+
+%?- '≤'([2], [3], Truth).
+%@    Truth = true.
+
+%?- '≤'([2,3], [3,2], Truth).
+%@    Truth = false.
+
+%?- '≤'([2], [3], true).
+%@    true.
+
+%?- '≤'([2], [3], false).
+%@    false.
+
+% Enumerates fairly, unlike ('≤')/3
+% TODO: Fairly enumerate ([0,0,0] '≤' Xs, label(Xs)); see below.
+Xs '≤' Ys :-
+    same_length(Xs, Ys), Ys \== [],
+    n_d_max(Ys, _, _),
+    Xs ins 0..1_000_000_000, % '0..sup'
+    maplist(\X^Y^(X #=< Y), Xs, Ys).
+
+%?- [] '≤' [].
+%@    false. % As desired
+
+%?- [2] '≤' [3].
+%@    true.
+
+%?- [3] '≤' [2].
+%@    false.
+
+%?- [2,3] '≤' [3,2].
+%@    false.
+
+%?- [2,X] '≤' [3,4].
+%@    clpz:(X in 0..4).
+
+%?- [2,3] '≤' [3,X].
+%@    clpz:(X in 3..1000000000), clpz:(#_A#>= #X), clpz:(#_A#=max(#X,3)), clpz:(_A in 3..1000000000).
+
+%?- [0,0,0] '≤' Xs.
+%@    Xs = [_D,_E,_C], clpz:(_A in 0..1000000000), clpz:(#_A#>= #_B), clpz:(#_A#>= #_C), clpz:(#_A#=max(#_C,#_B)), clpz:(_B in 0..1000000000), clpz:(#_B#>= #_D), clpz:(#_B#>= #_E), clpz:(#_B#=max(#_E,#_D)), clpz:(_D in 0..1000000000), clpz:(_E in 0..1000000000), clpz:(_C in 0..1000000000).
+
+%?- [0,0,0] '≤' Xs, label(Xs).
+%@    Xs = [0,0,0]
+%@ ;  Xs = [0,0,1]
+%@ ;  Xs = [0,0,2]
+%@ ;  Xs = [0,0,3]
+%@ ;  Xs = [0,0,4]
+%@ ;  Xs = [0,0,5]
+%@ ;  Xs = [0,0,6]
+%@ ;  Xs = [0,0,7]
+%@ ;  Xs = [0,0,8]
+%@ ;  Xs = [0,0,9]
+%@ ;  Xs = [0,0,10]
+%@ ;  Xs = [0,0,11]
+%@ ;  Xs = [0,0,12]
+%@ ;  ... . % UNFAIR
+
+%?- Xs '≤' [1,1,1], label(Xs).
+%@    Xs = [0,0,0]
+%@ ;  Xs = [0,0,1]
+%@ ;  Xs = [0,1,0]
+%@ ;  Xs = [0,1,1]
+%@ ;  Xs = [1,0,0]
+%@ ;  Xs = [1,0,1]
+%@ ;  Xs = [1,1,0]
+%@ ;  Xs = [1,1,1].
+
+
+%?- [1,1,1] '≤' Xs, Xs '≤' [2,2,2], label(Xs).
+%@    Xs = [1,1,1]
+%@ ;  Xs = [1,1,2]
+%@ ;  Xs = [1,2,1]
+%@ ;  Xs = [1,2,2]
+%@ ;  Xs = [2,1,1]
+%@ ;  Xs = [2,1,2]
+%@ ;  Xs = [2,2,1]
+%@ ;  Xs = [2,2,2].
+
+
+% TODO: Implement a fair-enumerating version of ('≰')/2 as well.
+Xs '≰' Ys :-
+    same_length(Xs, Ys),
+    length(Xs, D), D #> 0,
+    '≤'(Xs, Ys, false).
+
+% WRONG:
+%?- [1,1,1] '≰' Xs.
+%@    Xs = [_A,_B,_C], clpz:(_A in inf..0)
+%@ ;  Xs = [_A,_B,_C], clpz:(_A in 1..sup), clpz:(_B in inf..0)
+%@ ;  Xs = [_A,_B,_C], clpz:(_A in 1..sup), clpz:(_B in 1..sup), clpz:(_C in inf..0)
+%@ ;  false.
+
 n_d_sum(Ns, D, Sum) :-
     n_d_norm(Ns, D, Sum),
-    Ns ins 0..Sum,
-    sum(Ns, #=, Sum),
-    label(Ns).
+    sum(Ns, #=, Sum).
 
 n_d_max(Ns, D, Max) :-
     n_d_norm(Ns, D, Max),
-    Ns ins 0..Max,
-    label(Ns).
+    reduce(clpz:max_, Ns, Max).
 
 n_d_norm(Ns, D, Norm) :-
-    [I,D] ins 1..1_000, % '1..sup'
-    #I #= #Norm + #D, % index I penalizes Norm and D _jointly_
-    D #=< I, % sharply curtail growth of *dimension* D
-    label([I,D]),
-    length(Ns, D).
+    D    in 1..1_000, % '1..sup'
+    Norm in 0..1_000_000_000, % '0..sup'
+    (   nonvar(Ns) -> length(Ns, D)
+    ;   #I #= #Norm + #D,
+        label([I,D]),
+        length(Ns, D)
+    ),
+    maplist(\N^(#N #>= 0), Ns).
 
+% Fair enumeration fails if 1st arg is an improper list:
+%?- n_d_sum([3|Xs], D, Sum), label(Xs).
+%@    Xs = [], D = 1, Sum = 3
+%@ ;  Xs = [0], D = 2, Sum = 3
+%@ ;  Xs = [1], D = 2, Sum = 4
+%@ ;  Xs = [2], D = 2, Sum = 5
+%@ ;  Xs = [3], D = 2, Sum = 6
+%@ ;  Xs = [4], D = 2, Sum = 7
+%@ ;  ... .
 
-%?- L+\(findall(Ns, n_d_max(Ns, 2, 5), Nss), length(Nss, L)).
+%?- n_d_sum([3,X], D, Sum), label([X]).
+%@    X = 0, D = 2, Sum = 3
+%@ ;  X = 1, D = 2, Sum = 4
+%@ ;  X = 2, D = 2, Sum = 5
+%@ ;  X = 3, D = 2, Sum = 6
+%@ ;  ... .
+
+%?- n_d_max([3,X], D, Max).
+%@    D = 2, clpz:(X in 0..1000000000), clpz:(#Max#>= #X), clpz:(#Max#=max(#X,3)), clpz:(Max in 3..1000000000).
+
+%?- n_d_max(Ns, D, Max).
+%@    Ns = [0], D = 1, Max = 0
+%@ ;  Ns = [1], D = 1, Max = 1
+%@ ;  Ns = [0,0], D = 2, Max = 0
+%@ ;  Ns = [2], D = 1, Max = 2
+%@ ;  Ns = [_A,_B], D = 2, Max = 1, clpz:(_A in 0..1), clpz:(1#=max(#_B,#_A)), clpz:(_B in 0..1)
+%@ ;  Ns = [0,0,0], D = 3, Max = 0
+%@ ;  Ns = [3], D = 1, Max = 3
+%@ ;  Ns = [_A,_B], D = 2, Max = 2, clpz:(_A in 0..2), clpz:(2#=max(#_B,#_A)), clpz:(_B in 0..2)
+%@ ;  Ns = [_A,_C,_D], D = 3, Max = 1, clpz:(_A in 0..1), clpz:(#_B#>= #_A), clpz:(#_B#=max(#_C,#_A)), clpz:(_B in 0..1), clpz:(#_B#>= #_C), clpz:(1#=max(#_D,#_B)), clpz:(_C in 0..1), clpz:(_D in 0..1)
+%@ ;  ... .
+
+%?- n_d_max(Ns, D, Max), label(Ns).
+%@    Ns = [0], D = 1, Max = 0
+%@ ;  Ns = [1], D = 1, Max = 1
+%@ ;  Ns = [0,0], D = 2, Max = 0
+%@ ;  Ns = [2], D = 1, Max = 2
+%@ ;  Ns = [0,1], D = 2, Max = 1
+%@ ;  Ns = [1,0], D = 2, Max = 1
+%@ ;  Ns = [1,1], D = 2, Max = 1
+%@ ;  Ns = [0,0,0], D = 3, Max = 0
+%@ ;  Ns = [3], D = 1, Max = 3
+%@ ;  Ns = [0,2], D = 2, Max = 2
+%@ ;  Ns = [1,2], D = 2, Max = 2
+%@ ;  Ns = [2,0], D = 2, Max = 2
+%@ ;  Ns = [2,1], D = 2, Max = 2
+%@ ;  Ns = [2,2], D = 2, Max = 2
+%@ ;  Ns = [0,0,1], D = 3, Max = 1
+%@ ;  Ns = [0,1,0], D = 3, Max = 1
+%@ ;  ... .
+
+%?- n_d_max([3], D, Max).
+%@    D = 1, Max = 3.
+
+/*
+?- L+\(findall(Ns, (Max in 0..5,
+                    n_d_max(Ns, 2, Max),
+                    label(Ns)), Nss),
+       length(Nss, L)).
 %@    L = 36.
+*/
 
-%?- n_d_max(Ns, 2, 3).
-%@    Ns = [0,0]
-%@ ;  Ns = [0,1]
-%@ ;  Ns = [0,2]
-%@ ;  Ns = [0,3]
-%@ ;  Ns = [1,0]
-%@ ;  Ns = [1,1]
-%@ ;  Ns = [1,2]
-%@ ;  Ns = [1,3]
-%@ ;  Ns = [2,0]
-%@ ;  Ns = [2,1]
-%@ ;  Ns = [2,2]
-%@ ;  Ns = [2,3]
-%@ ;  Ns = [3,0]
-%@ ;  Ns = [3,1]
-%@ ;  Ns = [3,2]
-%@ ;  Ns = [3,3].
+%?- L+\(findall(Ns, (n_d_max(Ns, 2, 5), label(Ns)), Nss), length(Nss, L)).
+%@    L = 11.
+%?- L+\(setof(Ns, (n_d_max(Ns, 2, 5), label(Ns)), Nss), length(Nss, L)).
+%@    L = 11.
 
-
-%?- L+\(findall(Ns, n_d_sum(Ns, 2, 5), Nss), length(Nss, L)).
+%?- L+\(findall(Ns, (n_d_sum(Ns, 2, 5), label(Ns)), Nss), length(Nss, L)).
+%@    L = 6.
+%?- L+\(setof(Ns, (n_d_sum(Ns, 2, 5), label(Ns)), Nss), length(Nss, L)).
 %@    L = 6.
 
-%?- n_d_sum(Ns, 2, 5).
+%?- n_d_sum(Ns, 2, 5), label(Ns).
 %@    Ns = [0,5]
 %@ ;  Ns = [1,4]
 %@ ;  Ns = [2,3]
@@ -71,50 +234,29 @@ n_d_norm(Ns, D, Norm) :-
 %@ ;  Ns = [4,1]
 %@ ;  Ns = [5,0].
 
+%?- n_d_max(Ns, 2, 5), label(Ns).
+%@    Ns = [0,5]
+%@ ;  Ns = [1,5]
+%@ ;  Ns = [2,5]
+%@ ;  Ns = [3,5]
+%@ ;  Ns = [4,5]
+%@ ;  Ns = [5,0]
+%@ ;  Ns = [5,1]
+%@ ;  Ns = [5,2]
+%@ ;  Ns = [5,3]
+%@ ;  Ns = [5,4]
+%@ ;  Ns = [5,5].
+
 %?- I^D^Ns+\(#D #=< 3, #I #= #D + #Sum, n_d_sum(Ns, D, Sum)).
 %@    I = 1, D = 1, Ns = [0]
 %@ ;  I = 2, D = 1, Ns = [1]
 %@ ;  I = 2, D = 2, Ns = [0,0]
 %@ ;  I = 3, D = 1, Ns = [2]
-%@ ;  I = 3, D = 2, Ns = [0,1]
-%@ ;  I = 3, D = 2, Ns = [1,0]
+%@ ;  I = 3, D = 2, Ns = [_A,_B], clpz:(_A in 0..1), clpz:(#_A+ #_B#=1), clpz:(_B in 0..1)
 %@ ;  I = 3, D = 3, Ns = [0,0,0]
 %@ ;  I = 4, D = 1, Ns = [3]
-%@ ;  I = 4, D = 2, Ns = [0,2]
-%@ ;  I = 4, D = 2, Ns = [1,1]
-%@ ;  I = 4, D = 2, Ns = [2,0]
-%@ ;  I = 4, D = 3, Ns = [0,0,1]
-%@ ;  I = 4, D = 3, Ns = [0,1,0]
-%@ ;  I = 4, D = 3, Ns = [1,0,0]
-%@ ;  I = 5, D = 1, Ns = [4]
-%@ ;  I = 5, D = 2, Ns = [0,3]
-%@ ;  I = 5, D = 2, Ns = [1,2]
-%@ ;  I = 5, D = 2, Ns = [2,1]
-%@ ;  I = 5, D = 2, Ns = [3,0]
-%@ ;  I = 5, D = 3, Ns = [0,0,2]
-%@ ;  I = 5, D = 3, Ns = [0,1,1]
-%@ ;  I = 5, D = 3, Ns = [0,2,0]
-%@ ;  I = 5, D = 3, Ns = [1,0,1]
-%@ ;  I = 5, D = 3, Ns = [1,1,0]
-%@ ;  I = 5, D = 3, Ns = [2,0,0]
-%@ ;  I = 6, D = 1, Ns = [5]
-%@ ;  I = 6, D = 2, Ns = [0,4]
-%@ ;  I = 6, D = 2, Ns = [1,3]
-%@ ;  I = 6, D = 2, Ns = [2,2]
-%@ ;  I = 6, D = 2, Ns = [3,1]
-%@ ;  I = 6, D = 2, Ns = [4,0]
-%@ ;  I = 6, D = 3, Ns = [0,0,3]
-%@ ;  I = 6, D = 3, Ns = [0,1,2]
-%@ ;  I = 6, D = 3, Ns = [0,2,1]
-%@ ;  I = 6, D = 3, Ns = [0,3,0]
-%@ ;  I = 6, D = 3, Ns = [1,0,2]
-%@ ;  I = 6, D = 3, Ns = [1,1,1]
-%@ ;  I = 6, D = 3, Ns = [1,2,0]
-%@ ;  I = 6, D = 3, Ns = [2,0,1]
-%@ ;  I = 6, D = 3, Ns = [2,1,0]
-%@ ;  I = 6, D = 3, Ns = [3,0,0]
-%@ ;  I = 7, D = 1, Ns = [6]
-%@ ;  I = 7, D = 2, Ns = [0,5]
+%@ ;  I = 4, D = 2, Ns = [_A,_B], clpz:(_A in 0..2), clpz:(#_A+ #_B#=2), clpz:(_B in 0..2)
+%@ ;  I = 4, D = 3, Ns = [_A,_B,_C], clpz:(_A in 0..1), clpz:(#_A+ #_B+ #_C#=1), clpz:(_B in 0..1), clpz:(_C in 0..1)
 %@ ;  ... .
 
 %?- I^D^Ns+\(#I #< 6, #I #= #D + #Sum, n_d_sum(Ns, D, Sum)).
@@ -122,31 +264,15 @@ n_d_norm(Ns, D, Norm) :-
 %@ ;  I = 2, D = 1, Ns = [1]
 %@ ;  I = 2, D = 2, Ns = [0,0]
 %@ ;  I = 3, D = 1, Ns = [2]
-%@ ;  I = 3, D = 2, Ns = [0,1]
-%@ ;  I = 3, D = 2, Ns = [1,0]
+%@ ;  I = 3, D = 2, Ns = [_A,_B], clpz:(_A in 0..1), clpz:(#_A+ #_B#=1), clpz:(_B in 0..1)
 %@ ;  I = 3, D = 3, Ns = [0,0,0]
 %@ ;  I = 4, D = 1, Ns = [3]
-%@ ;  I = 4, D = 2, Ns = [0,2]
-%@ ;  I = 4, D = 2, Ns = [1,1]
-%@ ;  I = 4, D = 2, Ns = [2,0]
-%@ ;  I = 4, D = 3, Ns = [0,0,1]
-%@ ;  I = 4, D = 3, Ns = [0,1,0]
-%@ ;  I = 4, D = 3, Ns = [1,0,0]
+%@ ;  I = 4, D = 2, Ns = [_A,_B], clpz:(_A in 0..2), clpz:(#_A+ #_B#=2), clpz:(_B in 0..2)
+%@ ;  I = 4, D = 3, Ns = [_A,_B,_C], clpz:(_A in 0..1), clpz:(#_A+ #_B+ #_C#=1), clpz:(_B in 0..1), clpz:(_C in 0..1)
 %@ ;  I = 4, D = 4, Ns = [0,0,0,0]
 %@ ;  I = 5, D = 1, Ns = [4]
-%@ ;  I = 5, D = 2, Ns = [0,3]
-%@ ;  I = 5, D = 2, Ns = [1,2]
-%@ ;  I = 5, D = 2, Ns = [2,1]
-%@ ;  I = 5, D = 2, Ns = [3,0]
-%@ ;  I = 5, D = 3, Ns = [0,0,2]
-%@ ;  I = 5, D = 3, Ns = [0,1,1]
-%@ ;  I = 5, D = 3, Ns = [0,2,0]
-%@ ;  I = 5, D = 3, Ns = [1,0,1]
-%@ ;  I = 5, D = 3, Ns = [1,1,0]
-%@ ;  I = 5, D = 3, Ns = [2,0,0]
-%@ ;  I = 5, D = 4, Ns = [0,0,0,1]
-%@ ;  I = 5, D = 4, Ns = [0,0,1,0]
-%@ ;  I = 5, D = 4, Ns = [0,1,0,0]
-%@ ;  I = 5, D = 4, Ns = [1,0,0,0]
+%@ ;  I = 5, D = 2, Ns = [_A,_B], clpz:(_A in 0..3), clpz:(#_A+ #_B#=3), clpz:(_B in 0..3)
+%@ ;  I = 5, D = 3, Ns = [_A,_B,_C], clpz:(_A in 0..2), clpz:(#_A+ #_B+ #_C#=2), clpz:(_B in 0..2), clpz:(_C in 0..2)
+%@ ;  I = 5, D = 4, Ns = [_A,_B,_C,_D], clpz:(_A in 0..1), clpz:(#_A+ #_B+ #_C+ #_D#=1), clpz:(_B in 0..1), clpz:(_C in 0..1), clpz:(_D in 0..1)
 %@ ;  I = 5, D = 5, Ns = [0,0,0,0,0]
 %@ ;  false.
--- tally.pl
+++ tally.pl
@@ -15,27 +15,23 @@ clpz:monotonic.
 
 qs_d_nmax(Qs, D, Nmax) :-
     n_d_max(Ns, D, Nmax),
-    length(Ts, D),
+    label(Ns),
+    %%length(Ts, D),
     maplist(\T^N^(T in 0..N), Ts, Ns),
     label(Ts),
     maplist(\Q^T^N^(Q = T/N), Qs, Ts, Ns).
 
-%?- L+\(findall(Qs, qs_d_nmax(Qs, 2, 3), Qss), length(Qss, L)).
+%?- L+\(findall(Qs, (Max in 0..3, qs_d_nmax(Qs, 2, Max)), Qss), length(Qss, L)).
 %@    L = 100. % Correct: (4+3+2+1)^2
 
 %?- qs_d_nmax(Qs, D, Nmax). % MGQ
 %@    Qs = [0/0], D = 1, Nmax = 0
-%@ ;  Qs = [0/0], D = 1, Nmax = 1
 %@ ;  Qs = [0/1], D = 1, Nmax = 1
 %@ ;  Qs = [1/1], D = 1, Nmax = 1
 %@ ;  Qs = [0/0,0/0], D = 2, Nmax = 0
-%@ ;  Qs = [0/0], D = 1, Nmax = 2
-%@ ;  Qs = [0/1], D = 1, Nmax = 2
-%@ ;  Qs = [1/1], D = 1, Nmax = 2
 %@ ;  Qs = [0/2], D = 1, Nmax = 2
 %@ ;  Qs = [1/2], D = 1, Nmax = 2
 %@ ;  Qs = [2/2], D = 1, Nmax = 2
-%@ ;  Qs = [0/0,0/0], D = 2, Nmax = 1
 %@ ;  Qs = [0/0,0/1], D = 2, Nmax = 1
 %@ ;  Qs = [0/0,1/1], D = 2, Nmax = 1
 %@ ;  Qs = [0/1,0/0], D = 2, Nmax = 1
@@ -45,16 +41,37 @@ qs_d_nmax(Qs, D, Nmax) :-
 %@ ;  Qs = [1/1,0/1], D = 2, Nmax = 1
 %@ ;  Qs = [1/1,1/1], D = 2, Nmax = 1
 %@ ;  Qs = [0/0,0/0,0/0], D = 3, Nmax = 0
-%@ ;  Qs = [0/0], D = 1, Nmax = 3
-%@ ;  Qs = [0/1], D = 1, Nmax = 3
-%@ ;  Qs = [1/1], D = 1, Nmax = 3
-%@ ;  Qs = [0/2], D = 1, Nmax = 3
-%@ ;  Qs = [1/2], D = 1, Nmax = 3
-%@ ;  Qs = [2/2], D = 1, Nmax = 3
 %@ ;  Qs = [0/3], D = 1, Nmax = 3
 %@ ;  Qs = [1/3], D = 1, Nmax = 3
 %@ ;  Qs = [2/3], D = 1, Nmax = 3
 %@ ;  Qs = [3/3], D = 1, Nmax = 3
-%@ ;  Qs = [0/0,0/0], D = 2, Nmax = 2
-%@ ;  Qs = [0/0,0/1], D = 2, Nmax = 2
+%@ ;  Qs = [0/0,0/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/0,1/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/0,2/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/1,0/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/1,1/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/1,2/2], D = 2, Nmax = 2
+%@ ;  Qs = [1/1,0/2], D = 2, Nmax = 2
+%@ ;  Qs = [1/1,1/2], D = 2, Nmax = 2
+%@ ;  Qs = [1/1,2/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/2,0/0], D = 2, Nmax = 2
+%@ ;  Qs = [1/2,0/0], D = 2, Nmax = 2
+%@ ;  Qs = [2/2,0/0], D = 2, Nmax = 2
+%@ ;  Qs = [0/2,0/1], D = 2, Nmax = 2
+%@ ;  Qs = [0/2,1/1], D = 2, Nmax = 2
+%@ ;  Qs = [1/2,0/1], D = 2, Nmax = 2
+%@ ;  Qs = [1/2,1/1], D = 2, Nmax = 2
+%@ ;  Qs = [2/2,0/1], D = 2, Nmax = 2
+%@ ;  Qs = [2/2,1/1], D = 2, Nmax = 2
+%@ ;  Qs = [0/2,0/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/2,1/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/2,2/2], D = 2, Nmax = 2
+%@ ;  Qs = [1/2,0/2], D = 2, Nmax = 2
+%@ ;  Qs = [1/2,1/2], D = 2, Nmax = 2
+%@ ;  Qs = [1/2,2/2], D = 2, Nmax = 2
+%@ ;  Qs = [2/2,0/2], D = 2, Nmax = 2
+%@ ;  Qs = [2/2,1/2], D = 2, Nmax = 2
+%@ ;  Qs = [2/2,2/2], D = 2, Nmax = 2
+%@ ;  Qs = [0/0,0/0,0/1], D = 3, Nmax = 1
+%@ ;  Qs = [0/0,0/0,1/1], D = 3, Nmax = 1
 %@ ;  ... .
